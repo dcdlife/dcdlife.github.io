a03e6fb5b710b1f88cef4acd667a0c031396bd63
dcdlife <71340227+dcdlife@users.noreply.github.com>
2024-07-26 00:43:50 +0800

    增加背景图

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,15 +2,17 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import MyLayout from './MyLayout.vue'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  // Layout: () => {
+  //   return h(DefaultTheme.Layout, null, {
+  //     // https://vitepress.dev/guide/extending-default-theme#layout-slots
+  //   });
+  // },
+  Layout: MyLayout,
   enhanceApp({ app, router, siteData }) {
     // ...
   }
